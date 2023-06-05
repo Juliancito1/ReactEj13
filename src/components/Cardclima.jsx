e4108c94ecfa3700164c8ae6f69dbef7bbff396e
Julian <julianser@hotmail.com.ar>
2023-06-05 12:31:40 -0300

    Se agrego un alert de error al ingresar un pais o ciudad que no existe y se dio formato con prettier

--- src/components/Cardclima.jsx
+++ src/components/Cardclima.jsx
@@ -1,28 +1,31 @@
-import { Card, Container, Row , Col} from 'react-bootstrap';
+import { Card, Container, Row, Col } from "react-bootstrap";
 
-const Cardclima = ({clima}) => {
-   
-    return (
-        <Container className='d-flex justify-content-center my-3'>
-            <Row>
-            {clima.map((clima,indice) => (
-                <Col md={12} key={indice}>
-            <Card className='fondo'>
-                <Card.Header>
+const Cardclima = ({ clima }) => {
+  return (
+    <Container className="d-flex justify-content-center my-3">
+      <Row>
+        {Object.values(clima).length > 0 ? (
+          <Col md={12}>
+            <Card className="fondo">
+              <Card.Header>
                 <Card.Title>{clima.name}</Card.Title>
-                </Card.Header>
-                <Card.Body>
-                <h4 className=''>Temperatura: {Math.round(clima.main.temp)} °C</h4>
-                </Card.Body>
-                <Card.Footer>
+              </Card.Header>
+              <Card.Body>
+                <h4 className="">
+                  Temperatura: {Math.round(clima.main.temp)} °C
+                </h4>
+              </Card.Body>
+              <Card.Footer>
                 <h4>Descripcion: {clima.weather[0].description}</h4>
-                </Card.Footer>
+              </Card.Footer>
             </Card>
-            </Col>
-            ))}
-            </Row>
-        </Container>
-    );
+          </Col>
+        ) : (
+          <h2>No solicito el clima aún</h2>
+        )}
+      </Row>
+    </Container>
+  );
 };
 
 export default Cardclima;
